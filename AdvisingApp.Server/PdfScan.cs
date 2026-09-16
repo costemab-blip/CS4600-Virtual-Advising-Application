823c98a2a72fabe2463d8351f4a37afba61ee458
costemab <mcostello1271@gmail.com>
2026-09-16 11:02:43 -0500

    Added file to handle pdf scanning through pdfPig
    Add PdfScan class for PDF text extraction
    
    Added PdfScan.cs using UglyToad.PdfPig to extract and print words from PDF files. Updated Program.cs to call PdfScan.ScanPdf("string") before service configuration.

---
+++ AdvisingApp.Server/PdfScan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+public class PdfScan
+{
+    public static void ScanPdf(string filePath)
+    {
+        using (PdfDocument document = PdfDocument.Open(filePath))
+        {
+            foreach (Page page in document.GetPages())
+            {
+                IReadOnlyList<Letter> letters = page.Letters;
+                string example = string.Join(string.Empty, letters.Select(x => x.Value));
+
+                IEnumerable<Word> words = page.GetWords();
+                Console.WriteLine(words);
+            }
+        }
+    }
+}
--- AdvisingApp.Server/Program.cs
+++ AdvisingApp.Server/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+PdfScan.ScanPdf("string");
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
