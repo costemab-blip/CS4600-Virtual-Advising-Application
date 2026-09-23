9bbd1d84fea81d99467548f7f249fa8d05ebac27
costemab <mcostello1271@gmail.com>
2026-09-23 17:33:19 -0500

    added logic to create a UNICourseItem for each line in the txt file

--- AdvisingApp.Server/ClassData.cs
+++ AdvisingApp.Server/ClassData.cs
@@ -3,9 +3,13 @@
     public class UNICourseItem
     {
         public string? Title { get; set; }
-        public int? Units { get; set; }
+        public string? CourseTopic { get; set; }
+        public string? Year { get; set; }
+        public string? CourseCode { get; set; }
+        public string? Units { get; set; }
         public string? Grade { get; set; }
         public string? Term { get; set; }
         public string? Type { get; set; }
+
     }
 }
--- AdvisingApp.Server/InputReader.cs
+++ AdvisingApp.Server/InputReader.cs
@@ -1,19 +1,43 @@
-
+using AdvisingApp.Server;
+using System.Linq;
+using UglyToad.PdfPig.Content;
 public class InputReader
 {
     public static List<string> getWords(string filePath)
     {
-        List<string> wordList = new List<string>();       
+        List<string> wordList = new List<string>();
         var linesRead = File.ReadLines(filePath);
-    
+
         foreach (var line in linesRead)
         {
-            string[] words = line.Split([' ', '\n','\r'],
+            string[] words = line.Split([' ', '\n', '\r'],
             StringSplitOptions.RemoveEmptyEntries);
-            foreach ( var word in words)
+            List<string> tempList = new List<string>();
+            foreach (var word in words)
             {
-                wordList.Add(word);
+                tempList.Add(word);
             }
+
+            string title = "";
+            for (int Index = 2; Index < tempList.Count; Index++)
+            {
+                while (double.TryParse(tempList[Index], out _) == false)
+                {
+                    title += tempList[Index] + " ";
+                }
+                break;
+            }
+            UNICourseItem Course = new UNICourseItem()
+            {
+                CourseTopic = tempList[0],
+                CourseCode = tempList[1],
+                Title = title,
+                Units = tempList[tempList.Count - 5],
+                Term = tempList[tempList.Count - 3],
+                Year = tempList[tempList.Count - 4],
+                Grade = tempList[tempList.Count - 2],
+                Type = tempList[tempList.Count - 1],
+            };
         }
         return wordList;
     }
