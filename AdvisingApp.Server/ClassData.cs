04cbafbf1ede9820b38b42b3677422395c3a8a41
costemab <mcostello1271@gmail.com>
2026-09-23 12:37:21 -0500

    added UNICourseItem class, I am hoping to create an item for each course based off the the file that I scan through.

---
+++ AdvisingApp.Server/ClassData.cs
@@ -0,0 +1,11 @@
+﻿namespace AdvisingApp.Server
+{
+    public class UNICourseItem
+    {
+        public string? Title { get; set; }
+        public int? Units { get; set; }
+        public string? Grade { get; set; }
+        public string? Term { get; set; }
+        public string? Type { get; set; }
+    }
+}
